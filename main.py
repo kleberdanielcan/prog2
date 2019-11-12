94bc8d510b09d6ef7ed5a6d340356318e83ed157
kleberdanielcan <56438962+kleberdanielcan@users.noreply.github.com>
2019-11-12 14:45:31 +0100

    index.html aufgesetzt

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask("Telefonbuch")
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "test"
+    return render_template('index.html', name="Daniel")
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
